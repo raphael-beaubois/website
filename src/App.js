91428f4036878db58df7625c407959e0fc341e3a
raphael <34460710+raphaelBbs@users.noreply.github.com>
2021-04-27 11:48:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,31 +13,6 @@ import Skills from './components/Skills/Skills';
 import About from './components/About/About';
 import Project from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
-  
-
-// language = localStorage.getItem('lang'); 
-
-// state = {
-//   lang: this.language 
-// }
-
-
-// toggle = () => {
-
-//   const { lang } = this.state;
-//   this.setState({
-//     lang: !lang,
-//   });
-//   localStorage.setItem('lang', !lang)
-// }
-
-//   render(){
-//     //On le mets à false au départ??
-//     //oui Ok
-//    if(localStorage.getItem('lang') === null){
-//       localStorage.setItem('lang', false)
-//    }
-
 
 class App extends Component {
 
@@ -63,42 +38,35 @@ toggle = () => {
   return (
 
     <div className="App">
-<Switch>
+      <Switch>
 
-<Route exact path="/">
+      <Route exact path="/">
+      <Navigation  lang={lang} onButtonClick={this.toggle} />
+        <Header  lang={lang} onButtonClick={this.toggle} />           
+      </Route>
 
+      <Route exact path="/about">
+        <Navigation lang={lang} onButtonClick={this.toggle}/>
+          <About lang={lang} />                 
+      </Route>
 
-<Navigation  lang={lang} onButtonClick={this.toggle} />
-  <Header  lang={lang} onButtonClick={this.toggle} />    
-      
-</Route>
+      <Route exact path="/skills">
+        <Navigation lang={lang} onButtonClick={this.toggle}/>
+          <Skills  lang={lang}/>          
+      </Route>
 
-   <Route exact path="/about">
-<Navigation lang={lang} onButtonClick={this.toggle}/>
-  <About lang={lang} />  
-        
-   </Route>
+      <Route exact path="/project">
+        <Navigation lang={lang} onButtonClick={this.toggle}/>
+          <Project  lang={lang}/>                  
+      </Route>
 
-<Route exact path="/skills">
-<Navigation lang={lang} onButtonClick={this.toggle}/>
-  <Skills  lang={lang}/>   
-       
-   </Route>
+      <Route exact path="/contact">
+        <Navigation lang={lang} onButtonClick={this.toggle} />
+          <Contact lang={lang} />                    
+      </Route>
+            
 
-   <Route exact path="/project">
-<Navigation lang={lang} onButtonClick={this.toggle}/>
-  <Project  lang={lang}/> 
-         
-   </Route>
-
-    <Route exact path="/contact">
-  <Navigation lang={lang} onButtonClick={this.toggle} />
-    <Contact   lang={lang} /> 
-          
-    </Route>
-       
-
-  </Switch>
+        </Switch>
     </div>
   );
 }}
